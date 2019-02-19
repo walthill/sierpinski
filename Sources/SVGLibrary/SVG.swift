8d8670ed818ac2509d4fcf177fcc889f3f5c33fc
Walter Hill <walterhilldev@gmail.com>
2019-02-19 17:01:58 -0500

    Fixed string formatting.Need to test

--- Sources/SVGLibrary/SVG.swift
+++ Sources/SVGLibrary/SVG.swift
@@ -19,14 +19,15 @@ open class SVG {
         //TODO: work on converting values into string used to generate SVG file
         //Got it working partially with integer values but not with string values
         
-        //addContents(contentsToAdd: "<?xml version=\"1.0\" encodings=\"utf-8\"?>\n")
-        //addContents(contentsToAdd: "<svg version=\"1.1\" baseProfile=\"full\" width=\"%d\"height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\">\n", width, height);
+        addContents(contentsToAdd: "<?xml version=\"1.0\" encodings=\"utf-8\"?>\n")
+        addContents(contentsToAdd: "<svg version=\"1.1\" baseProfile=\"full\" width=\"\(width)\"height=\"\(height)\" xmlns=\"http://www.w3.org/2000/svg\">\n");
 
     }
     
     // YOUR CODE HERE
     public func drawLine(x1: Int, y1: Int, x2: Int, y2: Int, color: String) {
-        addContents(contentsToAdd: "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"%s\" />\n", x1, y1, x2, y2, color);
+        addContents(contentsToAdd: "<line x1=\"\(x1)\" y1=\"\(y1)\" x2=\"\(x2)\" y2=\"\(y2)\" stroke=\"\(color)\" />\n")
+        
     }
 
     //https://medium.com/@abhimuralidharan/variadic-functions-swift-54ce99a55c1d
@@ -38,5 +39,11 @@ open class SVG {
     public func write(filePath: String) {
         // YOUR CODE HERE
         addContents(contentsToAdd: "</svg>\n"); //see write_graphics in ssg.h
-    }
+        
+        contents.write(toFile: filePath, false, contents.utf8)
+        
+/*        write(toFile path: String,
+            atomically useAuxiliaryFile: Bool,
+            encoding enc: UInt) throws
+ */   }
 }
